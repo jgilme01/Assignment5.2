f554bfe6131d0a6c379e2efff6733490d4ab4aa6
jgilme01 <jgilme01@seattlecentral.edu>
2017-03-21 19:47:42 -0700

    Initial commit

---
+++ App_Code/CAService.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CAService" in code, svc and config file together.
+public class CAService : ICAService
+{
+    Community_AssistEntities CommAssEnt = new Community_AssistEntities();
+
+    public bool ApplyForGrant(GrantRequest gr)
+    {
+        bool result = true;
+        try
+        {
+            GrantReview review = new GrantReview();
+            review.GrantRequest= gr;
+            review.GrantRequestStatus = "pending";
+            CommAssEnt.GrantRequests.Add(gr);
+            //CommAssEnt.GrantReviews.Add(review);
+            CommAssEnt.SaveChanges();
+        }
+        catch(Exception ex)
+        {
+            
+            result = false;
+            throw ex;
+        }
+        return result;
+    }
+
+    public List<GrantInfo> GetGrantsByPerson(int personId)
+    {
+        var grants = from g in CommAssEnt.GrantRequests
+                     from r in g.GrantReviews
+                     where g.PersonKey == personId
+                     select new
+                     {
+                         g.GrantRequestDate,
+                         g.GrantRequestExplanation,
+                         g.GrantType.GrantTypeName,
+                         g.GrantRequestAmount,
+                         r.GrantRequestStatus
+
+                     };
+        List<GrantInfo> info = new List<GrantInfo>();
+        foreach(var gr in grants)
+        {
+            GrantInfo gi = new GrantInfo();
+            gi.GrantTypeName = gr.GrantTypeName;
+            gi.GrantRequestExplanation = gr.GrantRequestExplanation;
+            gi.GrantRequestAmount = (decimal)gr.GrantRequestAmount;
+            gi.GrantRequestStatus = gr.GrantRequestStatus;
+
+            info.Add(gi);
+        }
+        return info;
+
+    }
+
+    public List<GrantType> GetGrantTypes()
+    {
+        var types = from g in CommAssEnt.GrantTypes
+                     select new
+                     {
+                         g.GrantTypeKey,
+                         g.GrantTypeName,
+                     };
+        List<GrantType> gTypes = new List<GrantType>();
+        foreach(var t in types)
+        {
+            GrantType gt = new GrantType();
+            gt.GrantTypeKey = t.GrantTypeKey;
+            gt.GrantTypeName = t.GrantTypeName;
+            gTypes.Add(gt);
+        }
+        return gTypes;
+    }
+
+    public int PersonLogin(string user, string password)
+    {
+        int result = CommAssEnt.usp_Login(user, password);
+        int key = 0;
+        if(result != -1)
+        {
+            var uKey = (from p in CommAssEnt.People
+                        where p.PersonEmail.Equals(user)
+                        select p.PersonKey).FirstOrDefault();
+            key = (int)uKey;
+        }
+        
+        return key;
+    }
+
+    public bool RegisterPerson(PersonLite p)
+    {
+        bool result = true;
+        int successResult = CommAssEnt.usp_Register(p.LastName, p.FirstName, p.Email, p.Password, 
+            p.ApartmentNumber, p.Street, p.City, p.State, p.ZipCode, p.HomePhone, p.WorkPhone);
+        if(successResult == -1)
+        {
+            result = false;
+        }
+            return result;
+        
+    }
+}
---
+++ App_Code/ICAService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICAService" in both code and config file together.
+[ServiceContract]
+public interface ICAService
+{
+    [OperationContract]
+    int PersonLogin(string user, string password);
+
+    [OperationContract]
+    bool RegisterPerson(PersonLite p);
+
+    [OperationContract]
+    bool ApplyForGrant(GrantRequest gr);
+
+    [OperationContract]
+    List <GrantType> GetGrantTypes();
+
+    [OperationContract]
+    List<GrantInfo> GetGrantsByPerson(int personId);
+}
+
+[DataContract]
+public class PersonLite
+{//from StoredProcedures table, usp_Register members
+    [DataMember]
+    public string LastName { set; get; }
+
+    [DataMember]
+    public string FirstName { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+
+    [DataMember]
+    public string Password { set; get; }
+
+    [DataMember]
+    public string ApartmentNumber { set; get; }
+
+    [DataMember]
+    public string Street { set; get; }
+
+    [DataMember]
+    public string City { set; get; }
+
+    [DataMember]
+    public string State { set; get; }
+
+    [DataMember]
+    public string ZipCode { set; get; }
+
+    [DataMember]
+    public string HomePhone { set; get; }
+
+    [DataMember]
+    public string WorkPhone { set; get; }
+
+}
+
+[DataContract]
+public class GrantInfo
+{
+    [DataMember]//from GrantRequest table
+    public string GrantTypeName { set; get; }
+
+    [DataMember]//from GrantRequest table
+    public string GrantRequestExplanation { set; get; }
+
+    [DataMember]//from GrantRequest table
+    public decimal GrantRequestAmount { set; get; }
+
+    [DataMember]//from GrantReview table
+    public string GrantRequestStatus { set; get; }
+
+}
